a38e9614761d3fc705f8394e2c6a54c5849a0395
Алексей Бирев <birman126@gmail.com>
2022-05-18 10:39:13 +0400

    add HTML and CSS

---
+++ app.js
@@ -0,0 +1,8 @@
+var lampBtn = document.querySelector('.main-page-selection-lamp-box');
+lampBtn.onmouseover= function () {
+     
+    lampBtn.style.backgroundColor = 'white';
+    console.log(lampBtn)
+    
+}
+
